5a54a7d7f314ab72f94be3d51ef06da16412e44d
dkozak <dkozak94@gmail.com>
2019-04-08 14:28:30 +0100

    first attempt with custom styles

--- frontend-prototype-one/src/index.tsx
+++ frontend-prototype-one/src/index.tsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 
-const Search = () => <div>
-    <h1>Corproc search</h1>
-    <input type="text" placeholder="What do you want to know?"/>
+const SearchBar = () => {
+    const [query, setQuery] = useState('');
+
+    const onQueryChange = (newQuery: string) => {
+        setQuery(newQuery);
+    };
+
+    const inputSize = query.length > 20 ? query.length : 20;
+
+    return <div>
+        <input size={inputSize} type="text" placeholder="Search..." value={query}
+               onChange={e => onQueryChange(e.target.value)}/>
+    </div>
+}
+
+const Logo = () => <h1>Corproc search</h1>
+
+const Header = () => <div className="Header">
+    <h1>Header</h1>
 </div>
 
-ReactDOM.render(<Search/>, document.getElementById('root'));
+const App = () => {
+    return <div className="App">
+        <Header/>
+        <div className="search-bar-main">
+            <Logo/>
+            <SearchBar/>
+        </div>
+    </div>
+};
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
