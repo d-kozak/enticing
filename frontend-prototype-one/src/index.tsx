7bfefdbd869c95fe242b53d793d33d25505725da
dkozak <dkozak94@gmail.com>
2019-04-08 13:01:35 +0100

    remove the default create-react-app content

--- frontend-prototype-one/src/index.tsx
+++ frontend-prototype-one/src/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+
+const Search = () => <div>
+    <h1>Corproc search</h1>
+    <input type="text" placeholder="What do you want to know?"/>
+</div>
+
+ReactDOM.render(<Search/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
