2989be302f21a6f1749809f64191de7a7ce29aaa
dkozak <dkozak94@gmail.com>
2019-04-09 18:05:05 +0100

    login page, WIP

--- material-ui-prototype/src/App.tsx
+++ material-ui-prototype/src/App.tsx
@@ -8,6 +8,50 @@ import LinearProgress from "@material-ui/core/es/LinearProgress";
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Login from "./Login";
 import Unknown from "./Unknown";
+import {Field, Form, Formik} from "formik";
+import * as Yup from 'yup';
+import {TextField} from "formik-material-ui";
+
+
+const LoginSchema = Yup.object().shape({
+    login: Yup.string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required'),
+    password: Yup.string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required'),
+});
+
+
+const FormikTest = () => {
+
+    return <div>
+        <h1>Testing formik</h1>
+        <Formik
+            initialValues={{
+                login: '',
+                password: '',
+            }}
+            validationSchema={LoginSchema}
+            onSubmit={values => {
+                // same shape as initial values
+                console.log(values);
+            }}
+        >
+            {({isSubmitting}) => (
+                <Form>
+                    <Field variant="outlined" type="text" name="login" label="Login" component={TextField}/>
+                    <Field variant="outlined" type="text" name="password" label="Password" component={TextField}/>
+
+                    <button type="submit" disabled={isSubmitting}>Submit</button>
+                </Form>
+            )}
+        </Formik>
+    </div>
+};
+
 
 const App = () => {
     const [isLoggedId, setLoggedIn] = useState(false);
@@ -47,6 +91,9 @@ const App = () => {
             {showProgress && <LinearProgress color="secondary"/>}
 
             <Switch>
+
+                <Route path="/" component={FormikTest}/>
+
                 <Route path="/" exact component={() => <SearchBar startSearching={startSearching}/>}/>
                 <Route path="/login" component={Login}/>
                 <Route component={Unknown}/>
--- material-ui-prototype/src/Login.tsx
+++ material-ui-prototype/src/Login.tsx
@@ -1,21 +1,72 @@
 import createStyles from "@material-ui/core/es/styles/createStyles";
 import {WithStyles} from "@material-ui/core";
-import withStyles from "@material-ui/core/es/styles/withStyles";
+import {withStyles} from '@material-ui/core/styles';
+
+import Button from '@material-ui/core/Button';
 
 import React from 'react';
+import Paper from "@material-ui/core/es/Paper";
+import {Theme} from "@material-ui/core/es";
+import {Field, Form, Formik, FormikActions} from "formik";
 
-const styles = createStyles({});
+import * as Yup from 'yup';
+import {TextField} from "formik-material-ui";
+
+const styles = (theme: Theme) => createStyles({
+    centered: {
+        position: 'fixed',
+        top: '40%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)'
+    },
+    textField: {
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+    }
+});
 
 
 export interface LoginProps extends WithStyles<typeof styles> {
 
 }
 
-const Login = (props: LoginProps) => {
+interface FormProps {
+    login: string,
+    password: string
+}
 
-    return <div>
-        <h1>login</h1>
-    </div>
+const schema = Yup.object({
+    login: Yup.string().required(''),
+    password: Yup.string().required('')
+})
+
+const Login = (props: LoginProps) => {
+    const {classes} = props;
+
+    return <Paper className={classes.centered}>
+        <Formik
+            initialValues={{
+                login: '',
+                password: ''
+            }}
+            validationSchema={schema}
+            onSubmit={(values: FormProps, actions: FormikActions<FormProps>) => {
+                console.log(values);
+                console.log(schema.validate(values));
+                actions.setSubmitting(false);
+            }}
+        >
+            {({isSubmitting}: any) => (
+                <Form>
+                    <Field variant="outlined" type="text" name="login" label="Login" component={TextField}/>
+                    <Field variant="outlined" type="password" name="password" label="Password" component={TextField}/>
+                    <Button type="submit" disabled={isSubmitting}>
+                        Submit
+                    </Button>
+                </Form>
+            )}
+        </Formik>
+    </Paper>
 };
 
 export default withStyles(styles, {
