df57da08277c626b842e2c29582737c8ebad78d2
dkozak <dkozak94@gmail.com>
2019-04-12 21:45:44 +0100

    add button to show bigger context for given search

--- material-ui-prototype/src/components/searchresult/SearchResultItem.tsx
+++ material-ui-prototype/src/components/searchresult/SearchResultItem.tsx
@@ -2,11 +2,14 @@ import createStyles from "@material-ui/core/es/styles/createStyles";
 import {WithStyles} from "@material-ui/core";
 import withStyles from "@material-ui/core/es/styles/withStyles";
 
-import React from 'react';
+import React, {useState} from 'react';
 import {SearchResult} from "../../entities/SearchResult";
 import {Link as LinkIcon} from "@material-ui/icons";
 import IconButton from "@material-ui/core/IconButton";
 import Grid from "@material-ui/core/es/Grid";
+import Switch from "@material-ui/core/es/Switch";
+import FormControlLabel from "@material-ui/core/es/FormControlLabel";
+import Typography from "@material-ui/core/es/Typography";
 
 const styles = createStyles({
     root: {
@@ -21,11 +24,27 @@ export interface SearchResultItemProps extends WithStyles<typeof styles> {
 
 const SearchResultItem = (props: SearchResultItemProps) => {
     const {searchResult, classes} = props;
+
+    const [showMore, setShowMore] = useState(false);
+
     return <div className={classes.root}>
-        {searchResult.snippet}
+        <Typography variant="body1">
+            {showMore ? searchResult.largerText : searchResult.snippet}
+        </Typography>
         <Grid container justify="flex-end">
             <Grid item>
-                <IconButton onClick={() => window.location.href = searchResult.url}>
+                <FormControlLabel
+                    control={
+                        <Switch
+                            checked={showMore}
+                            onChange={e => setShowMore(e.target.checked)}
+                            value="checkedB"
+                            color="primary"
+                        />
+                    }
+                    label="With context"
+                />
+                <IconButton title='Source url' onClick={() => window.location.href = searchResult.url}>
                     <LinkIcon/>
                 </IconButton>
             </Grid>
