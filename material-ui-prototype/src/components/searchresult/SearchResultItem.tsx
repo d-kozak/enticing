678a4e8a9407aadab8b10db608e81e41a35c8740
dkozak <dkozak94@gmail.com>
2019-04-12 22:11:41 +0100

    finish initial style of SearchResultList

--- material-ui-prototype/src/components/searchresult/SearchResultItem.tsx
+++ material-ui-prototype/src/components/searchresult/SearchResultItem.tsx
@@ -4,16 +4,21 @@ import withStyles from "@material-ui/core/es/styles/withStyles";
 
 import React, {useState} from 'react';
 import {SearchResult} from "../../entities/SearchResult";
-import {Link as LinkIcon} from "@material-ui/icons";
-import IconButton from "@material-ui/core/IconButton";
 import Grid from "@material-ui/core/es/Grid";
 import Switch from "@material-ui/core/es/Switch";
 import FormControlLabel from "@material-ui/core/es/FormControlLabel";
 import Typography from "@material-ui/core/es/Typography";
+import Button from "@material-ui/core/es/Button";
+
+import {unstable_useMediaQuery as useMediaQuery} from '@material-ui/core/useMediaQuery';
+
 
 const styles = createStyles({
     root: {
         margin: '10px 0px'
+    },
+    showContextBtnGrid: {
+        flex: 1
     }
 });
 
@@ -27,27 +32,37 @@ const SearchResultItem = (props: SearchResultItemProps) => {
 
     const [showMore, setShowMore] = useState(false);
 
+    const isSmall = !useMediaQuery('(min-width:500px)');
+
+    const ShowContext = () => <FormControlLabel
+        className={classes.showContextBtnGrid}
+        control={
+            <Switch
+                checked={showMore}
+                onChange={e => setShowMore(e.target.checked)}
+                value="checkedB"
+                color="primary"
+            />
+        }
+        label="Full context"
+    />
+
     return <div className={classes.root}>
         <Typography variant="body1">
             {showMore ? searchResult.largerText : searchResult.snippet}
         </Typography>
-        <Grid container justify="flex-end">
-            <Grid item>
-                <FormControlLabel
-                    control={
-                        <Switch
-                            checked={showMore}
-                            onChange={e => setShowMore(e.target.checked)}
-                            value="checkedB"
-                            color="primary"
-                        />
-                    }
-                    label="With context"
-                />
-                <IconButton title='Source url' onClick={() => window.location.href = searchResult.url}>
-                    <LinkIcon/>
-                </IconButton>
-            </Grid>
+        <Grid container justify="flex-end" alignContent="center">
+
+            {isSmall ? <Grid container>
+                <ShowContext/>
+            </Grid> : <ShowContext/>}
+
+            <Button color="primary" size="small">Details</Button>
+
+            <Button size="small">Annotations</Button>
+
+            <Button size="small" onClick={() => document.location.href = searchResult.url}>Link</Button>
+
         </Grid>
     </div>
 };
