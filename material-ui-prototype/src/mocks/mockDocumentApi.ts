1c1d9a0dd912c5cbdbb4b562107b2dec49c0f602
dkozak <dkozak94@gmail.com>
2019-04-17 21:51:44 +0100

    add one annotation to the dummy document text as well

--- material-ui-prototype/src/mocks/mockDocumentApi.ts
+++ material-ui-prototype/src/mocks/mockDocumentApi.ts
@@ -4,6 +4,7 @@ import {documentLoadedAction} from "../actions/dialog/DocumentDialogAction";
 import {SearchResult} from "../entities/SearchResult";
 import {IndexedDocument} from "../entities/IndexedDocument";
 import {loremIpsumLong} from "./loremIpsum";
+import {EdSheeran} from "./mockAnnotations";
 
 
 export const mockDocumentRequested = (searchResult: SearchResult, dispatch: Dispatch) => {
@@ -12,8 +13,8 @@ export const mockDocumentRequested = (searchResult: SearchResult, dispatch: Disp
         const document: IndexedDocument = {
             body: {
                 text: loremIpsumLong,
-                annotations: new Map(),
-                positions: []
+                annotations: new Map([[1, EdSheeran]]),
+                positions: [{annotationId: 1, from: 99, to: 109}]
             },
             title: searchResult.url,
             url: searchResult.url
