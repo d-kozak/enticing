4bcbdc864f3b527a874e12eed10d7301f609f611
dkozak <dkozak94@gmail.com>
2019-05-06 17:14:25 +0100

    add CRUD methods to EnticingUserService

--- webserver/src/main/kotlin/cz/vutbr/fit/knot/enticing/webserver/service/EnticingUserService.kt
+++ webserver/src/main/kotlin/cz/vutbr/fit/knot/enticing/webserver/service/EnticingUserService.kt
@@ -1,5 +1,6 @@
 package cz.vutbr.fit.knot.enticing.webserver.service
 
+import cz.vutbr.fit.knot.enticing.webserver.entity.EnticingUser
 import cz.vutbr.fit.knot.enticing.webserver.repository.EnticingUserRepository
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
@@ -8,6 +9,19 @@ import org.springframework.stereotype.Service
 
 @Service
 class EnticingUserService(private val userRepository: EnticingUserRepository) : UserDetailsService {
+
     override fun loadUserByUsername(username: String): UserDetails = userRepository.findByLogin(username)
             ?: throw UsernameNotFoundException("User with login $username not found")
+
+    fun saveUser(newUser: EnticingUser) {
+        userRepository.save(newUser)
+    }
+
+    fun deleteUser(user: EnticingUser) {
+        userRepository.delete(user)
+    }
+
+    fun updateUser(user: EnticingUser) {
+        userRepository.save(user)
+    }
 }
--- webserver/src/test/kotlin/cz/vutbr/fit/knot/enticing/webserver/service/EnticingUserServiceTest.kt
+++ webserver/src/test/kotlin/cz/vutbr/fit/knot/enticing/webserver/service/EnticingUserServiceTest.kt
@@ -14,10 +14,10 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException
 internal class EnticingUserServiceTest {
 
     private val userRepositoryMock = mockk<EnticingUserRepository>()
+    private val userService = EnticingUserService(userRepositoryMock)
 
     @Test
-    fun loadUserByUsernameTest() {
-        val userService = EnticingUserService(userRepositoryMock)
+    fun `loadUserByUsername Test`() {
         every { userRepositoryMock.findByLogin("dkozak") } returns EnticingUser("dkozak")
         every { userRepositoryMock.findByLogin(not("dkozak")) } returns null
 
@@ -27,4 +27,36 @@ internal class EnticingUserServiceTest {
 
         verify(exactly = 2) { userRepositoryMock.findByLogin(any()) }
     }
+
+
+    @Test
+    fun `saveNewUser test`() {
+
+        every { userRepositoryMock.save(EnticingUser("cat123")) } returns EnticingUser()
+
+        val newUser = EnticingUser("cat123")
+        userService.saveUser(newUser)
+        verify(exactly = 1) { userRepositoryMock.save(EnticingUser("cat123")) }
+    }
+
+    @Test
+    fun `delete user test`() {
+        every { userRepositoryMock.delete(EnticingUser("123")) } returns Unit
+
+        val user = EnticingUser("123")
+        userService.deleteUser(user)
+
+        verify(exactly = 1) { userRepositoryMock.delete(EnticingUser("123")) }
+    }
+
+    @Test
+    fun `update user test`() {
+        every { userRepositoryMock.save(EnticingUser("abc")) } returns EnticingUser("abc")
+
+        val user = EnticingUser("abc")
+        userService.updateUser(user)
+
+        verify(exactly = 1) { userRepositoryMock.save(EnticingUser("abc")) }
+    }
+
 }
