6c0bcb92f2a6c7f7d2056102bb924851150b7b95
dkozak <dkozak94@gmail.com>
2019-05-06 17:27:05 +0100

    add another repository test exploring how .save method works

--- webserver/src/test/kotlin/cz/vutbr/fit/knot/enticing/webserver/repository/EnticingUserRepositoryTest.kt
+++ webserver/src/test/kotlin/cz/vutbr/fit/knot/enticing/webserver/repository/EnticingUserRepositoryTest.kt
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import java.util.*
 
 @ExtendWith(SpringExtension::class)
 @DataJpaTest
@@ -32,5 +33,20 @@ class EnticingUserRepositoryTest {
                 .isNotNull
                 .extracting { it!!.login }
                 .isEqualTo("dkozak")
+    }
+
+    @Test
+    fun `Verify that save updates entity when the same id is used`() {
+        val user = entityManager.persistFlushFind(EnticingUser("abc"))
+        assertThat(userRepository.findAll().size).isEqualTo(1)
+
+        val userInNewRequest = EnticingUser("bcd").apply { id = user.id }
+
+        userRepository.save(userInNewRequest)
+        assertThat(userRepository.findAll().size).isEqualTo(1)
+        assertThat(userRepository.findById(user.id))
+                .map { it.login }
+                .isEqualTo(Optional.of("bcd"))
+
     }
 }
