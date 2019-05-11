8d700ba92e301823e2ab663f6ad8e4cae3b1de40
dkozak <dkozak94@gmail.com>
2019-05-11 20:56:47 +0100

    add warning to the appbar when no search settings are available

--- frontend/src/components/appbar/EnticingAppBar.tsx
+++ frontend/src/components/appbar/EnticingAppBar.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import {withStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import createStyles from "@material-ui/core/es/styles/createStyles";
 import {WithStyles} from "@material-ui/core/es";
-import AppBarMenuButtons from "./AppBarButtonsRouter";
-import {connect} from "react-redux";
-import {AppState} from "../../reducers/RootReducer";
-import {logoutRequestAction} from "../../actions/UserActions";
 import {Link} from "react-router-dom";
 import SearchSettingsSelector from "./SearchSettingsSelector";
+import WarningIcon from "@material-ui/icons/Warning";
+import {logoutRequestAction} from "../../actions/UserActions";
+import withStyles from "@material-ui/core/es/styles/withStyles";
+import {connect} from "react-redux";
 import {isAdminSelector} from "../../reducers/selectors";
+import {AppState} from "../../reducers/RootReducer";
+import AppBarButtonsRouter from "./AppBarButtonsRouter";
+import {IconButton} from "@material-ui/core";
 
 
 const styles = createStyles({
@@ -31,7 +33,7 @@ export type EnticingAppBarProps =
 const EnticingAppBar = (props: EnticingAppBarProps) => {
     const {classes, user, isAdmin, handleLogout, searchSettings} = props;
 
-    const linkRef = (node: HTMLAnchorElement | null) => {
+    const clearDecorationsRef = (node: HTMLAnchorElement | null) => {
         if (node) {
             node.style.textDecoration = 'none';
             node.style.color = 'white';
@@ -42,14 +44,22 @@ const EnticingAppBar = (props: EnticingAppBarProps) => {
         <AppBar position="static">
             <Toolbar>
                 <div className={classes.grow}>
-                    <Link to="/" innerRef={linkRef}>
+                    <Link to="/" innerRef={clearDecorationsRef}>
                         <Typography variant="h6" color="inherit">
                             Enticing
                         </Typography>
                     </Link>
                 </div>
                 {searchSettings.length > 0 && <SearchSettingsSelector/>}
-                <AppBarMenuButtons user={user} handleLogout={handleLogout}/>
+                {searchSettings.length == 0 && <Link innerRef={clearDecorationsRef} to="/search-settings">
+                    <IconButton color="inherit">
+                        <WarningIcon/>
+                        <Typography style={{margin: '5px'}} color="textSecondary" variant="h6">No search settings
+                            available</Typography>
+                        <WarningIcon/>
+                    </IconButton>
+                </Link>}
+                <AppBarButtonsRouter user={user} handleLogout={handleLogout}/>
             </Toolbar>
         </AppBar>
     </div>
