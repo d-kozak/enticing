f7286fc13f60228c0989bc2b5ad4e2fca9a25817
dkozak <dkozak94@gmail.com>
2019-06-27 00:52:15 +0100

    query-processor: finish first prototype, needs to be tested

--- query-processor/src/main/kotlin/cz/vutbr/fit/knot/enticing/query/processor/QueryProcessor.kt
+++ query-processor/src/main/kotlin/cz/vutbr/fit/knot/enticing/query/processor/QueryProcessor.kt
@@ -2,41 +2,50 @@ package cz.vutbr.fit.knot.enticing.query.processor
 
 import cz.vutbr.fit.knot.enticing.dto.query.SearchQuery
 import cz.vutbr.fit.knot.enticing.dto.response.SearchResult
+import cz.vutbr.fit.knot.enticing.query.processor.request.MResult
 import cz.vutbr.fit.knot.enticing.query.processor.request.ServerInfo
-import cz.vutbr.fit.knot.enticing.query.processor.request.ServerResult
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.runBlocking
 
+fun process(searchQuery: SearchQuery, servers: List<ServerInfo>, contactServer: (searchQuery: SearchQuery, serverInfo: ServerInfo) -> MResult<SearchResult>): Map<String, List<MResult<SearchResult>>> = runBlocking {
+    val serversToCall = servers.toMutableList()
+    var collectedSnippetsCount = 0
+    val serverResults = mutableMapOf<String, MutableList<MResult<SearchResult>>>()
 
-fun process(searchQuery: SearchQuery, serverInfo: List<ServerInfo>): List<ServerResult> = runBlocking {
-    var servers = serverInfo.toMutableList()
+    while (serversToCall.isNotEmpty() && collectedSnippetsCount < searchQuery.snippetCount) {
+        val snippetsToCollect = searchQuery.snippetCount - collectedSnippetsCount
+        val lastResults = serversToCall
+                .mapIndexed { i, server -> splitSnippetCount(i, serversToCall.size, snippetsToCollect, searchQuery) to server }
+                .map { (query, server) ->
+                    async {
+                        server.address to contactServer(query, server)
+                    }
+                }.awaitAll()
 
-    val snippetCount = 0
-    val result = mutableListOf<ServerResult>()
-
-    while (servers.isNotEmpty() && snippetCount < searchQuery.snippetCount) {
-        val results = serverInfo.map {
-            async {
-                it.address to singleRequest(searchQuery, it)
+        serversToCall.clear()
+        for ((server, result) in lastResults) {
+            val resultsPerServer = serverResults[server] ?: mutableListOf()
+            if (result.isSuccess && result.value.snippets.isNotEmpty()) {
+                serversToCall.add(ServerInfo(server, result.value.offset))
+                collectedSnippetsCount += result.value.snippets.size
             }
-        }.awaitAll()
-
-        servers.clear()
-        for ((server, serverResult) in results) {
-
+            resultsPerServer.add(result)
+            serverResults[server] = resultsPerServer
         }
     }
-
-
-
-
-    result
+    serverResults
 }
 
-fun singleRequest(searchQuery: SearchQuery, serverInfo: ServerInfo): SearchResult {
-    TODO("Not implemented yet")
+fun splitSnippetCount(i: Int, serverCount: Int, wantedSnippets: Int, query: SearchQuery): SearchQuery {
+    var newSnippetCount = wantedSnippets / serverCount
+    val modulo = wantedSnippets % serverCount
+    if (modulo != 0 && i < modulo) {
+        newSnippetCount++
+    }
+    return query.copy(snippetCount = newSnippetCount)
 }
+
 
 
 
--- query-processor/src/main/kotlin/cz/vutbr/fit/knot/enticing/query/processor/request/ServerInfo.kt
+++ query-processor/src/main/kotlin/cz/vutbr/fit/knot/enticing/query/processor/request/ServerInfo.kt
@@ -1,10 +1,7 @@
 package cz.vutbr.fit.knot.enticing.query.processor.request
 
 import cz.vutbr.fit.knot.enticing.dto.query.Offset
-import cz.vutbr.fit.knot.enticing.dto.response.SearchResult
 import cz.vutbr.fit.knot.enticing.dto.utils.regex.urlRegex
-import javax.validation.Valid
-
 import javax.validation.constraints.NotEmpty
 import javax.validation.constraints.Pattern
 
@@ -14,13 +11,4 @@ data class ServerInfo(
         @field:Pattern(regexp = urlRegex)
         val address: String,
         val offset: Offset? = null
-)
-
-data class ServerResult(
-        @field:NotEmpty
-        @field:Pattern(regexp = urlRegex)
-        val server: String,
-        @field:Valid
-        val result: SearchResult?,
-        val error: String?
 )
