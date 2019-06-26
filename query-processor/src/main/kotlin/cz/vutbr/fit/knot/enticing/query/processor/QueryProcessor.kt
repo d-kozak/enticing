bb198ff9af2760cb804db1e3092968fe70aaf1ab
dkozak <dkozak94@gmail.com>
2019-06-26 18:52:51 +0100

    query-processor: start the implementation, WIP

--- query-processor/src/main/kotlin/cz/vutbr/fit/knot/enticing/query/processor/QueryProcessor.kt
+++ query-processor/src/main/kotlin/cz/vutbr/fit/knot/enticing/query/processor/QueryProcessor.kt
@@ -1,9 +1,42 @@
 package cz.vutbr.fit.knot.enticing.query.processor
 
 import cz.vutbr.fit.knot.enticing.dto.query.SearchQuery
+import cz.vutbr.fit.knot.enticing.dto.response.SearchResult
 import cz.vutbr.fit.knot.enticing.query.processor.request.ServerInfo
 import cz.vutbr.fit.knot.enticing.query.processor.request.ServerResult
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
 
-interface QueryProcessor {
-    fun process(searchQuery: SearchQuery, serverInfo: List<ServerInfo>): List<ServerResult>
+
+fun process(searchQuery: SearchQuery, serverInfo: List<ServerInfo>): List<ServerResult> = runBlocking {
+    var servers = serverInfo.toMutableList()
+
+    val snippetCount = 0
+    val result = mutableListOf<ServerResult>()
+
+    while (servers.isNotEmpty() && snippetCount < searchQuery.snippetCount) {
+        val results = serverInfo.map {
+            async {
+                it.address to singleRequest(searchQuery, it)
+            }
+        }.awaitAll()
+
+        servers.clear()
+        for ((server, serverResult) in results) {
+
+        }
+    }
+
+
+
+
+    result
 }
+
+fun singleRequest(searchQuery: SearchQuery, serverInfo: ServerInfo): SearchResult {
+    TODO("Not implemented yet")
+}
+
+
+
