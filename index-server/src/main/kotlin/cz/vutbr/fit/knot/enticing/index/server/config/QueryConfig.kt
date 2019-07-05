9ad7c2bba3564aa50e28d3d466a5a14c18044aa5
dkozak <dkozak94@gmail.com>
2019-07-05 18:56:35 +0100

    index-server: finish initiation of QueryExecutor

--- index-server/src/main/kotlin/cz/vutbr/fit/knot/enticing/index/server/config/QueryConfig.kt
+++ index-server/src/main/kotlin/cz/vutbr/fit/knot/enticing/index/server/config/QueryConfig.kt
@@ -9,21 +9,36 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.io.File
 
 
 @Configuration
 class QueryConfig(
-        @Value("\${config.file}") private val configFile: String
+        @Value("\${config.file}") private val configFile: String,
+        @Value("\${index.directory}") private val indexDirectory: String,
+        @Value("\${mg4j.files}") private val mg4jFiles: List<String>
 ) {
 
     private val log: Logger = LoggerFactory.getLogger(QueryConfig::class.java)
 
     @Bean
     fun queryExecutor(): QueryExecutor {
-        // todo allow to change the input indexed files
         log.info("Loading configuration from $configFile")
         val config = executeScript<IndexClientConfig>(configFile)
         log.info("Loaded config $config")
+
+        // todo move validation into the configuration
+
+        val indexDirectory = File(indexDirectory)
+        indexDirectory.isDirectory || throw IllegalArgumentException("${this.indexDirectory} is not a directory")
+        config.indexDirectory = indexDirectory
+        log.info("Using index directory ${this.indexDirectory}")
+
+        val inputFiles = mg4jFiles.map { File(it) }
+        val nonExistent = inputFiles.filter { !it.exists() }
+        nonExistent.isNotEmpty() && throw IllegalArgumentException("Files $nonExistent do not exist")
+        config.mg4jFiles = inputFiles
+        log.info("Using mg4j files ${this.mg4jFiles}")
         return initQueryExecutor(config)
     }
 }
--- index-server/src/test/kotlin/cz/vutbr/fit/knot/enticing/index/server/ExecuteMainMethodTest.kt
+++ index-server/src/test/kotlin/cz/vutbr/fit/knot/enticing/index/server/ExecuteMainMethodTest.kt
@@ -5,8 +5,12 @@ import org.junit.jupiter.api.Test
 class ExecuteMainMethodTest {
 
     @Test
-    fun `execute main method and see what happens`() {
+    fun `execute main method with proper arguments`() {
         // any argument in format --key=value is converted into a property and takes precedence over other property sources(e.g. application.properties)
-        main(arrayOf("--config.file=src/test/resources/client.config.kts"))
+        main(arrayOf(
+                "--config.file=src/test/resources/client.config.kts",
+                "--index.directory=../data/indexed",
+                "--mg4j.files=../data/mg4j/cc1.mg4j,../data/mg4j/cc2.mg4j,../data/mg4j/cc3.mg4j"
+        ))
     }
 }
