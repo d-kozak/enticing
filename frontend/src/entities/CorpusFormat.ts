b7261c0a7dc0617a3492b00f1563c6f2715a409c
dkozak <dkozak94@gmail.com>
2019-07-18 16:08:22 +0200

    frontend: add data type for corpus format, which is gonna be needed now

---
+++ frontend/src/entities/CorpusFormat.ts
@@ -0,0 +1,30 @@
+import * as yup from "yup";
+import {mapValues} from "lodash";
+import {validateOrNull} from "./validationUtils";
+
+
+type String2StringObjectMap = { [key: string]: string }
+
+export type IndexInfo = String2StringObjectMap
+
+export type AttributesInfo = String2StringObjectMap
+
+export interface CorpusFormat {
+    indexes: IndexInfo,
+    entities: { [clazz: string]: AttributesInfo }
+}
+
+const string2stringObjectMapSchema = yup.lazy(obj => yup.object(
+    mapValues(obj, () => yup.string().required())
+))
+
+export const corpusFormatSchema = yup.object({
+    indexes: string2stringObjectMapSchema,
+    entities: yup.lazy(obj => yup.object(
+        mapValues(obj, () => string2stringObjectMapSchema)
+    ))
+});
+
+export function isCorpusFormat(obj: object): obj is CorpusFormat {
+    return validateOrNull(corpusFormatSchema, obj) !== null
+}
