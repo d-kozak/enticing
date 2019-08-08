701a82e7dd67d042afef66604aa24f66fea3c700
dkozak <dkozak94@gmail.com>
2019-08-08 12:00:13 +0200

    frontend: removeUnused StateFromReducers type that was used to infer the ApplicationState from reducer return types, it is not used anymore, the shape of state is explicit now

--- frontend/src/reducers/StateFromReducers.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Infers state of a combined reducer based on return types of individual reducers
- */
-export type StateFromReducers<T> = {
-    [K in keyof T]: T[K] extends (...args: any) => any ? ReturnType<T[K]> : T[K]
-};
