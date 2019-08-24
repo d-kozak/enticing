e1019739fb889d2e342305f3456094ae023113d7
dkozak <dkozak94@gmail.com>
2019-08-24 15:05:46 +0200

    scripts: more updates

--- scripts/node/start_indexing.sh
+++ scripts/node/start_indexing.sh
@@ -2,3 +2,4 @@
 [[ -d "$ENTICING_HOME" ]] || { echo "ENTICING_HOME not defined" >&2; exit 1; }
 MEM=$("$ENTICING_HOME"/scripts/helpers/print_mem.sh)
 echo "I've got $MEM mb of free ram"
+echo "REMOTE HOME IS $REMOTE_HOME"
--- scripts/start_indexing.sh
+++ scripts/start_indexing.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 [[ -d "$ENTICING_HOME" ]] || { echo "ENTICING_HOME not defined" >&2; exit 1; }
-parallel-ssh -l xkozak15 -h "$ENTICING_HOME"/scripts/servers/athenas.txt -i "uname -a"
+export REMOTE_HOME="/mnt/minerva1/nlp/projects/corpproc_search/cloned/enticing"
+parallel-ssh -l xkozak15 -h "$ENTICING_HOME"/scripts/servers/athenas.txt -i "$REMOTE_HOME/scripts/node/start_indexing.sh"
