f18c9d83b1ad018c4cfa6612117e61e231f314df
dkozak <dkozak94@gmail.com>
2019-08-24 15:34:55 +0200

    scripts: update start_indexing script

--- scripts/node/start_indexing.sh
+++ scripts/node/start_indexing.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-[[ -d "$ENTICING_HOME" ]] || { echo "ENTICING_HOME not defined" >&2; exit 1; }
+[[ -d "$ENTICING_HOME" ]] || { echo "ENTICING_HOME not defined, trying to configure it" >&2; export ENTICING_HOME=realpath "$BASH_SOURCE/../.."; echo "ENTICING_HOME set to $ENTICING_HOME"; }
 MEM=$("$ENTICING_HOME"/scripts/helpers/print_mem.sh)
 echo "I've got $MEM mb of free ram"
 echo "REMOTE HOME IS $REMOTE_HOME"
