bad2faaadfcf2ff9350fab1ec882aa73b06a45b3
dkozak <dkozak94@gmail.com>
2019-09-02 21:46:19 +0200

    global: update kotlin to 1.3.50 and remove dependency to kts-runner -> it is is possible to execute scripts without it

--- dto/src/main/kotlin/cz/vutbr/fit/knot/enticing/dto/config/loader.kt
+++ dto/src/main/kotlin/cz/vutbr/fit/knot/enticing/dto/config/loader.kt
@@ -1,12 +1,14 @@
 package cz.vutbr.fit.knot.enticing.dto.config
 
-import de.swirtz.ktsrunner.objectloader.KtsObjectLoader
 import java.io.File
+import javax.script.ScriptEngineManager
 
 /**
  * Executes kotlin script on the defined path and returns it's result cast to a specific type
  */
 inline fun <reified Result> executeScript(path: String): Result {
     val script = File(path).readText()
-    return KtsObjectLoader().load(script)
+    val engine = ScriptEngineManager().getEngineByExtension("kts")
+            ?: throw IllegalStateException("Could not locate kts engine")
+    return engine.eval(script) as Result
 }
