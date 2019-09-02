4ac4af2e321818d308387a0690e54fb549895072
dkozak <dkozak94@gmail.com>
2019-09-02 21:50:30 +0200

    global: new index-server starter script

--- scripts/debug/index-server.sh
+++ scripts/debug/index-server.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-[[ -d "$ENTICING_HOME" ]] || { echo "ENTICING_HOME not defined" >&2; exit 1; }
-"${ENTICING_HOME}"/scripts/index-server.sh --config.file="${ENTICING_HOME}"/index-server/src/test/resources/client.config.kts --mg4j.files=foo --index.directory=bar  --server.port=8001
+ENTICING_HOME=$("$(dirname "$BASH_SOURCE")"/../utils/get_enticing_home.sh)
+retval=$?
+if [[ "$retval" -ne "0" ]] ; then
+  exit $retval
+fi
+"${ENTICING_HOME}"/bin/index-server --config.file="${ENTICING_HOME}"/index-server/src/test/resources/client.config.kts --mg4j.files=foo --index.directory=bar  --server.port=8001
--- scripts/index-server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-[[ -d "$ENTICING_HOME" ]] || { echo "ENTICING_HOME not defined" >&2; exit 1; }
-java -jar "${ENTICING_HOME}"/lib/index-server-0.0.1-SNAPSHOT.jar "$@"
