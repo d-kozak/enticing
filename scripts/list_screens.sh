2fcbc8b3aa077a011238f8665d09df68f1dbbaa7
dkozak <dkozak94@gmail.com>
2019-09-03 17:09:54 +0200

    scripts: update list screen script

--- scripts/list_screens.sh
+++ scripts/list_screens.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 [[ -d "$ENTICING_HOME" ]] || { echo "ENTICING_HOME not defined" >&2; exit 1; }
 [[ -n "$1" ]] || { echo "Expecting a server file as argument" >&2; exit 1; }
-#parallel-ssh -l xkozak15 -h "$1" -i "screen -ls"
-parallel-ssh -l xkozak15 -h "$1" -i "ls -l"
+parallel-ssh -l xkozak15 -h "$1" -i "screen -ls"
