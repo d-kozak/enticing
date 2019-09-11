7dc9bbec8399c1ab9acc92e79483b109eb56f1f0
dkozak <dkozak94@gmail.com>
2019-09-11 16:28:00 +0200

    scripts: more updates in the deploy/reboot script

--- deploy/1.0_wiki_first_experiment/reboot.sh
+++ deploy/1.0_wiki_first_experiment/reboot.sh
@@ -11,9 +11,9 @@ webserver_screen_name="enticing-webserver"
 webserver_logfile="/mnt/data/indexes/xkozak15/wiki/webserver.log"
 
 cd "$ENTICING_HOME" || exit 1
-./scripts/kill_screens.sh "$ENTICING_HOME"/deploy/1.0_wiki_first_experiment/martin_limit.txt || exit 1
-ssh "xkozak15@$webserver_server screen -S $webserver_screen_name -X quit" || exit 1
-echo "1/5 All running screens were killed"
+./scripts/kill_screens.sh "$ENTICING_HOME"/deploy/1.0_wiki_first_experiment/martin_limit.txt
+ssh "xkozak15@$webserver_server" "screen -S $webserver_screen_name -X quit"
+echo "1/5 All running enticing screens were killed"
 gradle buildAll || exit 1
 echo "2/5 Project build finished"
 jars=$(ls lib/*.jar)
@@ -21,6 +21,6 @@ scp $jars "xkozak15@$webserver_server:$remote_home/lib/" || exit 1
 echo "3/5 Jars copied"
 ./deploy/1.0_wiki_first_experiment/start_index_servers.sh || exit 1
 echo "4/5 Index servers started"
-ssh "xkozak15@$webserver_server cd $remote_home && screen -S $webserver_screen_name -d -m ./bin/webserver && screen -S $webserver_screen_name -X logfile $webserver_logfile && screen -S $webserver_screen_name -X log" || exit 1
+ssh "xkozak15@$webserver_server" "cd $remote_home && screen -S $webserver_screen_name -d -m ./bin/webserver && screen -S $webserver_screen_name -X logfile $webserver_logfile && screen -S $webserver_screen_name -X log" || exit 1
 echo "5/5 Webserver started"
 echo "FINISHED"
