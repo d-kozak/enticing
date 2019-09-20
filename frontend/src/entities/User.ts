7c687a4af46ecde673a4c04df3322a89c76f902f
dkozak <dkozak94@gmail.com>
2019-09-20 11:10:07 +0200

    frontend - set the default number of results per page to 10, 5 is too little

--- frontend/src/entities/User.ts
+++ frontend/src/entities/User.ts
@@ -17,7 +17,7 @@ export const createAnonymousUser: () => User = () => ({
     login: "",
     roles: [],
     userSettings: {
-        resultsPerPage: 5
+        resultsPerPage: 10
     },
     selectedSettings: null,
     selectedMetadata: {}
