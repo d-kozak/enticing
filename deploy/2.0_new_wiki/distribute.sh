60c0936d6ef4ac4425a657c1c91eca260f514ca4
dkozak <dkozak94@gmail.com>
2019-11-12 15:46:57 +0100

    deploy 2.0: prepare file distribution scripts

---
+++ deploy/2.0_new_wiki/distribute.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ENTICING_HOME=$("$(dirname "$BASH_SOURCE")"/../../scripts/utils/get_enticing_home.sh)
+retval=$?
+if [[ "$retval" -ne "0" ]]; then
+  exit $retval
+fi
+
+cd "$ENTICING_HOME/scripts" || exit 1
+python3.6 distribute_corpus.py /var/xdolez52/Zpracovani_Wikipedie/html_from_wikipedia_en_all_novid_2018-10.zim/6-mg4j /mnt/minerva1/nlp/projects/corpproc_search/corpproc_search /mnt/data/indexes/xkozak15/new_wiki
