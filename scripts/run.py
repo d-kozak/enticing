bd4a4c557730e59fc9baa7f5dccaae68450a6a23
dkozak <dkozak94@gmail.com>
2019-11-19 17:06:21 +0100

    deploy 2.0: add webserver/index-server start & kill, not tested yet

--- scripts/run.py
+++ scripts/run.py
@@ -10,7 +10,7 @@ from check_mg4j_files import show_all_mg4j_files
 from distribute_corpus import distribute_corpus
 from start_indexing import start_indexing
 from utils.config_validation import verify_config
-from utils.utils import execute_via_ssh
+from utils.utils import execute_via_ssh, execute_parallel_ssh
 
 
 def print_config(config: configparser.ConfigParser) -> None:
@@ -38,9 +38,12 @@ def parse_arguments() -> argparse.Namespace:
     parser.add_argument("-d", "--distrib", action='store_true', help="Distribute mg4j files over servers")
     parser.add_argument("-i", "--index", action='store_true', help="Index mg4j files using index-builder")
     parser.add_argument("-r", "--reboot", action='store_true', help="Reboot index servers and webserver")
+    parser.add_argument("-w", "--webserver", action='store_true', help="Reboot webserver only")
     parser.add_argument("-k", "--kill", action='store_true', help="Kill all screens")
     namespace = parser.parse_args()
-    if not any([namespace.clean, namespace.show, namespace.distrib, namespace.index, namespace.reboot, namespace.kill]):
+    if not any(
+            [namespace.clean, namespace.show, namespace.distrib, namespace.index, namespace.reboot, namespace.webserver,
+             namespace.kill]):
         print("At least one operation is required", file=sys.stderr)
         parser.print_help()
         sys.exit(1)
@@ -73,13 +76,52 @@ def execute_show(conf: configparser.ConfigParser) -> None:
     show_all_mg4j_files(mg4j_dir, servers)
 
 
-def execute_index(conf: configparser.ConfigParser) -> None:
+def execute_start_indexing(conf: configparser.ConfigParser) -> None:
     mg4j_dir = conf["common"]["mg4j_dir"]
     kts_config = conf["index-builder"]["config"]
     user = conf["common"]["username"]
     servers = conf["common"]["servers_file"]
     enticing_home = conf["common"]["enticing_home"]
     start_indexing(mg4j_dir, kts_config, enticing_home, servers, user)
+
+
+def execute_index_server_start(conf: configparser.ConfigParser) -> None:
+    servers = conf["common"]["servers_file"]
+    user = conf["common"]["username"]
+    index_screen = conf["index-server"]["screen_name"]
+    index_port = conf["index-server"]["port"]
+    index_conf = conf["index-server"]["config"]
+    index_log = conf["index-server"]["log"]
+    enticing_home = conf["common"]["enticing_home"]
+    mg4j_dir = conf["common"]["mg4j_dir"]
+    cmd = f"/opt/anaconda3/bin/python3.6 {enticing_home}/scripts/node/start_server.py {mg4j_dir} {index_conf} {index_screen} {index_port} {index_log}"
+    execute_parallel_ssh(servers, user, cmd)
+
+
+def execute_index_server_kill(conf: configparser.ConfigParser) -> None:
+    servers = conf["common"]["servers_file"]
+    user = conf["common"]["username"]
+    index_screen = conf["index-server"]["screen_name"]
+    cmd = f"screen -S {index_screen} -X quit"
+    execute_parallel_ssh(servers, user, cmd)
+
+
+def execute_webserver_start(conf: configparser.ConfigParser) -> None:
+    user = conf["common"]["username"]
+    enticing_home = conf["common"]["enticing_home"]
+    webserver_server = conf["webserver"]["server"]
+    webserver_screen_name = conf["webserver"]["screen_name"]
+    webserver_log = conf["webserver"]["log"]
+    cmd = f"cd {enticing_home} && screen -S {webserver_screen_name} -d -m ./bin/webserver && screen -S {webserver_screen_name} -X logfile {webserver_log} && screen -S {webserver_screen_name} -X log"
+    execute_via_ssh(webserver_server, user, cmd)
+
+
+def execute_webserver_kill(conf: configparser.ConfigParser) -> None:
+    user = conf["common"]["username"]
+    webserver_server = conf["webserver"]["server"]
+    webserver_screen_name = conf["webserver"]["screen_name"]
+    cmd = f"screen -S {webserver_screen_name} -X quit"
+    execute_via_ssh(webserver_server, user, cmd)
 
 
 def main():
@@ -91,6 +133,10 @@ def main():
         sys.exit(1)
     print_config(conf)
 
+    if args.kill or args.reboot:
+        execute_index_server_kill(conf)
+        execute_webserver_kill(conf)
+
     if args.clean:
         execute_clean(conf)
 
@@ -101,7 +147,15 @@ def main():
         execute_show(conf)
 
     if args.index:
-        execute_index(conf)
+        execute_start_indexing(conf)
+
+    if args.webserver:
+        execute_webserver_kill(conf)
+        execute_webserver_start(conf)
+
+    if args.reboot:
+        execute_index_server_start(conf)
+        execute_webserver_start(conf)
 
 
 if __name__ == "__main__":
