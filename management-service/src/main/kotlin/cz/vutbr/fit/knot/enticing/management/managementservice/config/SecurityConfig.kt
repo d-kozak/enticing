5a0fc43841312e3bccc17216307171f433a205a1
dkozak <dkozak94@gmail.com>
2020-02-12 19:40:09 +0100

    management-service: temporarily disable security

---
+++ management-service/src/main/kotlin/cz/vutbr/fit/knot/enticing/management/managementservice/config/SecurityConfig.kt
@@ -0,0 +1,19 @@
+package cz.vutbr.fit.knot.enticing.management.managementservice.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@Configuration
+class SecurityConfig : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity) {
+        http.csrf()
+                .disable()
+                .cors()
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .permitAll()
+    }
+}
