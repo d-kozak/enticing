b81d7ad223f4fe2bb455ab6f57245f26271b99ab
dkozak <dkozak94@gmail.com>
2020-02-12 19:06:11 +0100

    global: add management-service module

---
+++ management-service/src/main/kotlin/cz/vutbr/fit/knot/enticing/management/managementservice/ManagementServiceApplication.kt
@@ -0,0 +1,11 @@
+package cz.vutbr.fit.knot.enticing.management.managementservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ManagementServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ManagementServiceApplication>(*args)
+}
---
+++ management-service/src/test/kotlin/cz/vutbr/fit/knot/enticing/management/managementservice/ManagementServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package cz.vutbr.fit.knot.enticing.management.managementservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ManagementServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
