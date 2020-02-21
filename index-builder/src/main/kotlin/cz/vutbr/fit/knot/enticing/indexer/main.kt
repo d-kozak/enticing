2b85674cacad3127b25fd6f394c20306c03071a1
dkozak <dkozak94@gmail.com>
2020-02-21 20:58:38 +0100

    index-builder: process the collections sequentially (its very resource consuming)

--- index-builder/src/main/kotlin/cz/vutbr/fit/knot/enticing/indexer/main.kt
+++ index-builder/src/main/kotlin/cz/vutbr/fit/knot/enticing/indexer/main.kt
@@ -6,9 +6,6 @@ import cz.vutbr.fit.knot.enticing.dto.config.dsl.validateOrFail
 import cz.vutbr.fit.knot.enticing.dto.config.executeScript
 import cz.vutbr.fit.knot.enticing.index.startIndexing
 import cz.vutbr.fit.knot.enticing.log.configureFor
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 fun handleArguments(vararg args: String, loadConfig: (path: String) -> EnticingConfiguration = ::executeScript): Pair<EnticingConfiguration, String> {
     args.size == 2 || throw IllegalArgumentException("format: /path/to/config.kts serverAddress")
@@ -16,25 +13,25 @@ fun handleArguments(vararg args: String, loadConfig: (path: String) -> EnticingC
     return config to args[1]
 }
 
-suspend fun main(args: Array<String>) {
+fun main(args: Array<String>) {
     val (enticingConfiguration, address) = handleArguments(*args)
     val config = enticingConfiguration.indexServerByAddress(address)
 
     val logger = enticingConfiguration.loggingConfiguration.configureFor("$address-builder")
 
     logger.measure("indexing") {
-        withContext(Dispatchers.Default) {
-            for ((collection, mg4jDir, indexDir) in config.loadCollections()) {
-                launch {
+        for ((collection, mg4jDir, indexDir) in config.loadCollections()) {
+            try {
+                logger.measure("collection ${collection.name}") {
                     val builderConf = IndexBuilderConfig(config.corpus.name, collection.name, mg4jDir, indexDir, config.metadataConfiguration
                             ?: config.corpus.metadataConfiguration)
-                    try {
-                        startIndexing(builderConf)
-                    } catch (ex: java.lang.Exception) {
-                        ex.printStackTrace()
-                    }
+
+                    startIndexing(builderConf)
                 }
+            } catch (ex: java.lang.Exception) {
+                ex.printStackTrace()
             }
         }
     }
 }
+
