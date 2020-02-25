673b4f8e9b8b889d238ca092a8e90cc9ab313cee
dkozak <dkozak94@gmail.com>
2020-02-25 19:29:56 +0100

    global: ensure all spring webservices are deployable

--- management-service/src/main/kotlin/cz/vutbr/fit/knot/enticing/management/managementservice/config/GlobalConfig.kt
+++ management-service/src/main/kotlin/cz/vutbr/fit/knot/enticing/management/managementservice/config/GlobalConfig.kt
@@ -1,8 +1,6 @@
 package cz.vutbr.fit.knot.enticing.management.managementservice.config
 
 import cz.vutbr.fit.knot.enticing.dto.config.dsl.EnticingConfiguration
-import cz.vutbr.fit.knot.enticing.dto.config.dsl.IndexServerConfiguration
-import cz.vutbr.fit.knot.enticing.dto.config.dsl.metadata.MetadataConfiguration
 import cz.vutbr.fit.knot.enticing.dto.config.dsl.validateOrFail
 import cz.vutbr.fit.knot.enticing.dto.config.dsl.visitor.prettyPrint
 import cz.vutbr.fit.knot.enticing.dto.config.executeScript
@@ -36,12 +34,4 @@ class GlobalConfig(
     fun logger(enticingConfiguration: EnticingConfiguration): MeasuringLogService {
         return enticingConfiguration.loggingConfiguration.configureFor(address)
     }
-
-    @Bean
-    fun indexServerConfiguration(config: EnticingConfiguration): IndexServerConfiguration = config.indexServerByAddress(address)
-
-    @Bean
-    fun metadataConfiguration(config: IndexServerConfiguration): MetadataConfiguration = config.metadataConfiguration
-            ?: config.corpus.metadataConfiguration
-
 }
