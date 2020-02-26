aa6112a8aea48faf70df6b80f98cbb2dc35ee0cb
dkozak <dkozak94@gmail.com>
2020-02-26 20:22:00 +0100

    index-server: add remote logging

--- index-server/src/main/kotlin/cz/vutbr/fit/knot/enticing/index/server/controller/GlobalControllerExceptionHandler.kt
+++ index-server/src/main/kotlin/cz/vutbr/fit/knot/enticing/index/server/controller/GlobalControllerExceptionHandler.kt
@@ -1,16 +1,23 @@
 package cz.vutbr.fit.knot.enticing.index.server.controller
 
 import cz.vutbr.fit.knot.enticing.eql.compiler.EqlCompilerException
+import cz.vutbr.fit.knot.enticing.log.MeasuringLogService
+import cz.vutbr.fit.knot.enticing.log.logger
+import cz.vutbr.fit.knot.enticing.log.util.error
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import javax.servlet.http.HttpServletResponse
 
+
 @ControllerAdvice
-class GlobalControllerExceptionHandler {
+class GlobalControllerExceptionHandler(logService: MeasuringLogService) {
+
+    val logger = logService.logger { }
 
     @ExceptionHandler(EqlCompilerException::class)
     fun eqlCompilerException(exception: EqlCompilerException, response: HttpServletResponse) {
+        logger.error(exception)
         response.sendError(HttpStatus.BAD_REQUEST.value(), exception.message)
     }
 }
