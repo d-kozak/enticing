a87d8d15afbaa40e34be2099a9894d0976bf9684
dkozak <dkozak94@gmail.com>
2020-03-01 04:06:54 +0100

    dto: send logs asynchronously to the Management server

--- dto/src/main/kotlin/cz/vutbr/fit/knot/enticing/api/ManagementServiceApi.kt
+++ dto/src/main/kotlin/cz/vutbr/fit/knot/enticing/api/ManagementServiceApi.kt
@@ -1,6 +1,10 @@
 package cz.vutbr.fit.knot.enticing.api
 
 import cz.vutbr.fit.knot.enticing.log.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 
 private const val API_BASE_PATH = "/api/v1"
 
@@ -8,17 +12,23 @@ private const val API_BASE_PATH = "/api/v1"
  * Wrapper around the api of the management service
  */
 class ManagementServiceApi(remoteAddress: String, componentType: ComponentType, localAddress: String, loggerFactory: LoggerFactory)
-    : EnticingComponentApi(remoteAddress, componentType, localAddress, loggerFactory), RemoteLoggingApi {
+    : EnticingComponentApi(remoteAddress, componentType, localAddress, loggerFactory), RemoteLoggingApi, AutoCloseable {
+
+    private val scope = CoroutineScope(Dispatchers.IO)
 
     override fun log(logMessage: LogMessage) {
-        httpPost("$API_BASE_PATH/log", logMessage.toLogDto(localAddress, componentType))
+        scope.launch { httpPost("$API_BASE_PATH/log", logMessage.toLogDto(localAddress, componentType)) }
     }
 
     override fun perf(perfMessage: PerfMessage) {
-        httpPost("$API_BASE_PATH/perf", perfMessage.toPerfDto(localAddress, componentType))
+        scope.launch { httpPost("$API_BASE_PATH/perf", perfMessage.toPerfDto(localAddress, componentType)) }
     }
 
     fun heartbeat() {
         httpPost("$API_BASE_PATH/heartbeat", HeartbeatDto(localAddress, componentType))
+    }
+
+    override fun close() {
+        scope.cancel()
     }
 }
