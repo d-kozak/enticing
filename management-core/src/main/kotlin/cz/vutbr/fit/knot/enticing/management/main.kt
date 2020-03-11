2167278efbc548c91a3c626073e41d3275296759
dkozak <dkozak94@gmail.com>
2020-03-11 11:50:44 +0100

    management-core: finish basic cli management

--- management-core/src/main/kotlin/cz/vutbr/fit/knot/enticing/management/main.kt
+++ management-core/src/main/kotlin/cz/vutbr/fit/knot/enticing/management/main.kt
@@ -1,10 +1,22 @@
 package cz.vutbr.fit.knot.enticing.management
 
+import cz.vutbr.fit.knot.enticing.log.loggerFactoryFor
+import cz.vutbr.fit.knot.enticing.log.measure
+
 fun main(args: Array<String>) {
     runManagementCli(args)
 }
 
 fun runManagementCli(args: Array<String>) {
-    val args = parseCliArgs(args)
-    println(args)
+    println("Management-cli: starting with args ${args.contentToString()}")
+    val args = parseCliArgs(args).validateOrFail()
+    val configuration = args.configuration
+    val loggerFactory = configuration.loggingConfiguration.loggerFactoryFor("management-cli")
+    val logger = loggerFactory.namedLogger("Management-cli")
+    val engine = ManagementEngine(configuration, loggerFactory)
+    logger.measure("executeCliApp", args.toString()) {
+        engine.use {
+            engine.execute(args)
+        }
+    }
 }
