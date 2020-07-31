55b94c21f6dd0fda3d34bf6d9daa7a5d632651c8
dkozak <dkozak94@gmail.com>
2020-07-31 16:18:41 +0200

    server-probe: finish documentation pass

--- server-probe/src/main/kotlin/cz/vutbr/fit/knot/enticing/server/probe/main.kt
+++ server-probe/src/main/kotlin/cz/vutbr/fit/knot/enticing/server/probe/main.kt
@@ -3,7 +3,10 @@ package cz.vutbr.fit.knot.enticing.server.probe
 import cz.vutbr.fit.knot.enticing.dto.utils.toJson
 import cz.vutbr.fit.knot.enticing.mx.ServerProbe
 
-
+/**
+ * Just print current server status
+ * Used to get server info for servers on which no component is running
+ */
 fun main() = println(ServerProbe().scan().toJson())
 
 
